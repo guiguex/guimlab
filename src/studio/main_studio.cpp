27c4598add5476844614d61365c7ff2f448ee892
Guillaume Meingan <guillaume@guig.dev>
2026-08-31 17:11:09 -0400

    feat(studio): switch cockpit graphics backend to pure native Vulkan (SOKOL_VULKAN) on Ubuntu/Linux and clean all legacy GL/X11 dependencies

--- src/studio/main_studio.cpp
+++ src/studio/main_studio.cpp
@@ -11,10 +11,8 @@
 #define SOKOL_IMPL
 #if defined(_WIN32)
 #define SOKOL_D3D11
-#elif defined(__APPLE__)
-#define SOKOL_METAL
 #else
-#define SOKOL_GLCORE
+#define SOKOL_VULKAN
 #endif
 
 #include "sokol/sokol_app.h"
